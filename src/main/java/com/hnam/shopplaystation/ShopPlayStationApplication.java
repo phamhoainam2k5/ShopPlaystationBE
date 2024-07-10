7b452291844fe020d868904aa308bc6e9b1eba27
phamhoainam2k5 <phahoainam2005@gmail.com>
2024-07-10 08:35:44 +0700

    first commit

---
+++ src/main/java/com/hnam/shopplaystation/ShopPlayStationApplication.java
@@ -0,0 +1,13 @@
+package com.hnam.shopplaystation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopPlayStationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopPlayStationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hnam/ShopPlayStation/ShopPlayStationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hnam.ShopPlayStation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopPlayStationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
